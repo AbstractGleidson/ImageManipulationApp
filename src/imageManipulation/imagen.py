89f4fa2aa6ac21a8e46386d9b981268df5f6f7f9
AbstractGleidson <gleidsonabstract.evgm@gmail.com>
2025-12-08 18:19:40 -0300

    imageManipulation: image saving implemented

--- src/imageManipulation/imagen.py
+++ src/imageManipulation/imagen.py
@@ -1,8 +1,12 @@
 import cv2 as openCv  
 import numpy
 from pathlib import Path
+import os
 
 class InvalidFormatImage(Exception):
+    pass
+
+class SaveImageError(Exception):
     pass
  
 class Image:
@@ -10,18 +14,30 @@ class Image:
         if not self.validationFormat(image_path):
             raise InvalidFormatImage("A imagem deve ser .jpg ou .png") 
         
-        self.imagem_data = numpy.array(openCv.imread(image_path))
-        self.BASEPAHT = Path(__file__).parent.parent / "assets" / "Image"
+        data = openCv.imread(str(image_path))
+        
+        if data is None:
+            raise FileNotFoundError("Não foi possivel carregar a imagem!")
+        else:
+            self.imagem_data = numpy.array(data)
+        
+        self.nameImage = os.path.basename(image_path)
+        self.BASEPAHT = Path(__file__).parent.parent.parent / "assets" / "Imagens"
         
     def imageGrayScale(self):
         grayImage = openCv.cvtColor(self.imagem_data, openCv.COLOR_BGR2GRAY) # Converte a imagem pra escala de cinza 
             
+        self.saveImage(grayImage, "gray")
+        
+    def _imageGrayScale(self):
+        grayImage = openCv.cvtColor(self.imagem_data, openCv.COLOR_BGR2GRAY) # Converte a imagem pra escala de cinza 
+    
         return numpy.array(grayImage)
     
     def binaryImage(self):
         
         # Convete para escala de cinza
-        gray_image = self.imageGrayScale()
+        gray_image = self._imageGrayScale()
         
         # Convete para binario 
         binary_image = numpy.array(gray_image > 128).astype("uint8")
@@ -29,14 +45,17 @@ class Image:
         # Faz uma nova quantização
         binary_image *= 255
         
-        return binary_image
+        self.saveImage(binary_image, "binary")
     
     def negativeImage(self):
-        return -self.imagem_data
+        
+        negative_image = -self.imagem_data
+        
+        self.saveImage(negative_image, "negative")
     
     def cartoonImage(self):
         
-        gray_image = self.imageGrayScale()
+        gray_image = self._imageGrayScale()
         
         edges = openCv.adaptiveThreshold(
             gray_image, 
@@ -51,9 +70,26 @@ class Image:
         
         cartoon = openCv.bitwise_and(color, color, mask = edges)
         
-        return cartoon
+        self.saveImage(cartoon, "cartoon")
     
     def validationFormat(self, path_image: str):
-        extension = path_image.split(".")[-1]
+        extension = str(path_image).split(".")[-1]
         
         return (extension == "jpg" or extension == "png")
+    
+    def saveImage(self, image, filter: str):
+        imageName = f"{filter}_{self.nameImage}"
+        savePath = str(self.BASEPAHT / imageName)
+        
+        if openCv.imwrite(savePath, image):
+            return f"Imagem salva em: {savePath}"
+        else:
+            raise SaveImageError("Não foi possivel salvar a imagem!") 
+
+# if __name__ == "__main__":
+    
+#     p = Path(__file__).parent.parent.parent / "assets" / "Imagens" / "image.jpg"
+     
+#     image = Image(p)
+
+#     image.cartoonImage()    
